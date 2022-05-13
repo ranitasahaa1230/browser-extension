a5189ba7ab9d67c0b7611d8f3159b59b36fb2616
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-14 01:52:40 +0530

    update quotes and weather

---
+++ src/components/Weather.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+export const Weather = () => {
+  return (
+    <div>Weather</div>
+  )
+}
