06c3cdfd23acbd5303f3141d65fd7d579b5de37d
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2022-05-15 00:38:53 +0530

    Revert "User onboarding"

--- src/components/GetTimeAndDate.jsx
+++ src/components/GetTimeAndDate.jsx
@@ -1,39 +1,9 @@
-import React, { useEffect } from "react";
-import { useBrowser } from "../contexts";
+import React from 'react'
 
 export const GetTimeAndDate = () => {
-  const {
-    browserState: { time, greetings, userName },
-    browserDispatch,
-  } = useBrowser();
-
-  useEffect(() => {
-    getTime();
-    //eslint-disable-next-line
-  }, [time]);
-
-  const getTime = () => {
-    const today = new Date();
-    const getHour = today.getHours();
-    const getMins=today.getMinutes();
-    const curTime= (getHour < 10 ? "0" : '') + getHour + ":" + (getMins < 10 ? "0" : '') + getMins;
-    setTimeout(getTime, 1000);
-    browserDispatch({
-      type: "GETTIME",
-      payload: curTime,
-    });
-    browserDispatch({
-      type: "GREETINGS",
-      payload: getHour,
-    });
-  };
-
   return (
     <div>
-      <h1 className="text-9xl">{time}</h1>
-      <h1 className="m-5 text-6xl py-2 px-4">
-        {greetings}, {userName}.
-      </h1>
+    <h1>get Time</h1>
     </div>
-  );
-};
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import { Focus, GetTimeAndDate, Quotes, Weather } from "../components";
+import { GetTimeAndDate } from "../components";
+import { useBrowser } from "../contexts";
 
 export const HomePage = () => {
+  const {
+    browserState: { userName },
+  } = useBrowser();
   return (
-    <div className="text-center text-white font-semibold  h-screen flex justify-center	items-center flex-col	">
-      <span>
-        <Weather />
-      </span>
-
-      <span className="text-3xl">
-        <GetTimeAndDate />
-      </span>
-
-      <span>
-        <Focus />
-      </span>
-
-      <span className="m-6">
-        <Quotes />
-      </span>
-
+    <div className="text-center text-white font-bold  h-screen flex justify-center	items-center flex-col	">
+        <span className="text-3xl"><GetTimeAndDate /></span>
+      <h1 className="m-5 text-4xl">Welcome, {userName} !!</h1>
     </div>
   );
 };
